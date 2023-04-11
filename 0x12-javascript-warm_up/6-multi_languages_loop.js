dc0d420bbfcdc6341168e22345e18dda3a4023c3
Emmanuel Daudu <114646340+EmmylsonTech@users.noreply.github.com>
2023-04-11 08:51:38 +0100

    task done

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// script to print strings using a loop
+
+let x;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (x = 0; x < 3; x++) {
+  console.log(myString[x]);
+}
